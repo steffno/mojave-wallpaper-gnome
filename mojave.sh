b7a028e449a9f7d1638df646fc6af63e7a4f118f
steffno <49401703+steffno@users.noreply.github.com>
2021-12-08 19:21:45 +0100

    Changed Files and xml for BIgSur Wallpapers

---
+++ mojave.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Installing Mojave animated wallpaper..."
+mkdir /home/$USER/.mojave
+sed -i "s@\$USER@$USER@" mojave.xml
+mv mojave.xml /home/$USER/.mojave/mojave.xml
+mv wallpapers /home/$USER/.mojave/wallpapers
+gsettings set org.gnome.desktop.background picture-uri "file:/home/$USER/.mojave/mojave.xml"
+echo "Done!"
